0818d7a9444bdfc6ffb9ebc32cb011c2542c1af1
tanmaysandbhor2007 <tanmaysandbhor20@gmail.com>
2025-09-10 10:08:48 +0530

    Fix date string issue (use toISOString instead of typo)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
       title: title,
       content: value,
       _id: pasteId || Date.now().toString(36), // unique id
-      createdAt: new Date().toLocaltring(),
+      createdAt: new Date().toISOString(),
     };
 
     // console.log("Paste created:", paste);
